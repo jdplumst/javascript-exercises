a281cb6e8c8416e7797203f327377b885f4b08ce
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-18 02:35:07 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        let x = arr.indexOf(args[i]);
+        if (x != -1) {
+            arr.splice(x,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
