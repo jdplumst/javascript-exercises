507df6d8fc9fb18ab90f235af627a134b6a05ac7
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-18 02:14:21 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newStringArray = [];
+    originalStringArray = string.split('');
+    for (let i = originalStringArray.length - 1; i >= 0; i--) {
+        newStringArray.push(originalStringArray[i]);
+    }
+    let newString = newStringArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
