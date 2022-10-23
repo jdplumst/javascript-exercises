50f43534d45b3a5d95702818f4ab36ea6a263da3
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-22 23:54:09 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let backwardsString = ''
+    s = s.replace(/[^a-zA-Z]/g, '');
+    s = s.toLowerCase();
+    for (let i = s.length - 1; i >= 0; i--) {
+        backwardsString += s[i];
+    }
+    return backwardsString === s;
 };
 
 // Do not edit below this line
