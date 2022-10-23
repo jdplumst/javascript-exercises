287dbfdf18718b27162c1ebaf3253975578b8f18
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-23 00:22:47 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestIndex = 0;
+    let oldestAge = 0;
+    let currAge = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].hasOwnProperty('yearOfDeath')) {
+            arr[i].yearOfDeath = new Date().getFullYear();
+        } 
+        currAge = (arr[i].yearOfDeath - arr[i].yearOfBirth);
+        if (currAge > oldestAge) {
+            oldestIndex = i;
+            oldestAge = currAge;
+        }
+    }
+    return arr[oldestIndex];
 };
 
 // Do not edit below this line
