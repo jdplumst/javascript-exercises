1545475abc215a03224b26c8d9335b0095f90001
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-10-23 00:02:24 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    a = 1;
+    b = 1;
+    let curr = 2;
+    if (n == 1 || n == 2) {
+        return 1;
+    } else if (n == 3) {
+        return curr;
+    } else if (n <= 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i < n; i++) {
+        curr = a + b;
+        a = b;
+        b = curr;
+    }
+    return curr;
 };
 
 // Do not edit below this line
